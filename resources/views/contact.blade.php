e7545e642be035eb4c4f5753647d047cc9274ad0
francescoSvara <francesco.svara@gmail.com>
2023-03-02 22:10:50 +0100

    second commit

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col-12">
         <h1>Contatti</h1>
-        <p>Inostri contatti, seguici o contattaci tramite questa pagina</p>
+        <p>I nostri contatti, seguici o contattaci tramite questa pagina</p>
       </div>    
     </div>                    
   </div>
